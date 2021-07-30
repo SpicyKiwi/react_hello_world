b25d1868c99e1f8e32a48a50c3caa6338d9c8a4e
frosty <farhan.konsulting@gmail.com>
2021-07-30 16:00:53 -0400

    created a static TicTacToe board

--- src/TicTacToe.js
+++ src/TicTacToe.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import './TicTacToe.css'
 
-const board = [
-  'x', null, 'o', 
-  'o', 'x', null,
-  'x', 'o', null
-];
+const board = [ 'x', null, 'o', 'o', 'x', null,'x', 'o', null ];
 
 const TicTacToe = () => {
-  return <div className="tic-tac-toe">
-    <h1>Tic-Tac-Toe</h1>
-  </div>
+  return (<div className="tic-tac-toe">
+        <h1>Tic-Tac-Toe</h1>
+        <div className="board">
+          {board.map((char, index) => {
+            return <div className="cell" key={index}><span>{char}</span></div>
+          })}
+        </div>
+        </div>)
 }
 
 export default TicTacToe;
