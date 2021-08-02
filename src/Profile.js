55620c89568937b3dcafe1fff5c16ef2f8a9a48e
frosty <farhan.konsulting@gmail.com>
2021-08-02 11:33:05 -0400

    updated profile section(custom info)

--- src/Profile.js
+++ src/Profile.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import pfp from './img/default_pfp.jpg'
 
-const username = 'your username'//prompt('What is your username?')
-const email = 'your email' //prompt('What is your email?')
-const state = 'your state' //prompt('What state do you live in?')
-const city = 'your city' //prompt('What city do you live in?')
-const addy = 'your addy' //prompt('What is your address?')
+const username = prompt('What is your username?')
+const email = prompt('What is your email?')
+const state = prompt('What state do you live in?')
+const city = prompt('What city do you live in?')
+const addy = prompt('What is your address?')
 
 
 const Profile = () => {
